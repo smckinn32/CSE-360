7ee981890e3e6cc5acf99ddaba2c5164d06f9f9a
laithjas <73775765+laithjas@users.noreply.github.com>
2022-03-15 23:29:33 -0700

    Initial commit for the profile package
    Contains some code to create instances of users and admins, not complete.
    include a TestProfiles.java class to test the code.
    Users.csv file contains a default admin user that is called in the test code.
    This is not the final version.

--- src/profiles/Admin.java
+++ src/profiles/Admin.java
@@ -1,1 +1,29 @@
 package profiles;
+
+public class Admin extends Profile {
+
+    public Admin() {
+        super();
+        isAdmin = true;
+    }
+    public Admin(String userName, String passWrod,
+                 String fName, String lName, String addr, String number) {
+        super(userName, passWrod, fName, lName, addr, number);
+        isAdmin = true;
+
+    }
+
+    //TODO: write block code for this method
+    public void setCoupon() {
+
+    }
+
+    public void editMenu(){
+
+    }
+
+    public void swapView(){
+
+    }
+
+}
---
+++ src/profiles/TestProfiles.java
@@ -0,0 +1,39 @@
+/*
+    This is a test program that will test commonU, profile, admin classes.
+*/
+
+
+package profiles;
+
+import profiles.*;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestProfiles {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner stdin = new Scanner(System.in);
+
+        System.out.println("Enter your userName, password, First name, last name, address, and phone number:  ");
+        String uName = stdin.nextLine();
+        String pwd = stdin.nextLine();
+        String fisrtName = stdin.nextLine();
+        String lastName = stdin.nextLine();
+        String addr = stdin.nextLine();
+        String phone = stdin.nextLine();
+
+        //creating a common user using constructor param
+        CommonU user1 = new CommonU(uName, pwd, fisrtName, lastName, addr, phone);
+
+        //getting user1 info from instance
+        String[] str1 = user1.getUserInfo();
+        for(String s: str1) System.out.println(s);
+
+        // Creating a common user using default constructor
+        Admin user2 = new Admin();
+        // getting the data for user 2 from the txt file
+        String[] str2 = user2.getUserData("AdminUser", "Password123");
+        for(String s: str2) System.out.println(s);
+    }
+
+}
