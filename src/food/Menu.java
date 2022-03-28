89ffd1119a3ae8229263c8f8aa5161865c821c3d
smckinn32 <seanmckinnon32@gmail.com>
2022-03-28 12:41:42 -0700

    Finished search functions, completed menu builder & added a method for recording new dishes

--- src/food/Menu.java
+++ src/food/Menu.java
@@ -1,24 +1,54 @@
 package food;
 
 import java.util.*;
+import.java.io.*;
 
 public class Menu {
 
   ArrayList<Dish> menu = new ArrayList<Dish>();
+  String menuFile = "Menu.txt"
+  /* Builds the menu ArrayList by parsing through a text file.*/
+  public static void buildMenu(){
+    String ingredients, tags;
+    String[] ingredientList, tagList;
+    private int numDishes, timeToMake;
+    private double price;
+    private String dName;
 
-  //Will create the menu from a .txt file within the package
-  public static void buildMenu(){}
+    try {
+      Scanner scan = new Scanner(new FileInputStream(menuFile))
+      numDishes = Integer.parseInt(scan.nextLine());
+      scan.useDelimiter("|")
+
+      while(numDishes != 0){
+        dName = scan.next();
+        price = Double.parseDouble(scan.next());
+        timeToMake = Integer.parseInt(scan.next());
+        ingredients = scan.next();
+        tags = scan.next();
+
+        ingredientList = ingredients.split(",", 10);
+        tagList = tags.split(",", 5);
+
+        Dish newDish = new Dish(ingredientList, tagList, dName, price, timeToMake);
+        menu.add(newDish);
+
+        numDishes--;
+      }
+    } catch(IOException e);
+    scan.close();
+  }
 
   /*  Searches the menu for all dishes that contain the specified tags.
     It does this by searching the menu with a while loop, and for each dish it compares
     each tag to the searched tags. If a match is found it is added to the list and returned.*/
-  public static LinkedList<Dish> showMe(String[] searchTags){
+  public static LinkedList<Dish> searchByTag(String[] searchTags){
     LinkedList<Dish> foundDishes = new LinkedList<Dish>();
     String[] dishTags = new String[];
-    int menuSize = menu.size() - 1;
+    int counter = menu.size() - 1;
 
-    while(menuSize >= 0){
-      Dish currentDish = menu.get(menuSize);
+    while(counter >= 0){
+      Dish currentDish = menu.get(counter);
       dishTags = currentDish.getTags();
 
       for(int x = 0; x < searchTags.length; x++){
@@ -28,16 +58,50 @@ public class Menu {
           }
         }
       }
-      menuSize--;
+      counter--;
     }
     return foundDishes;
+  }
+
+  public static void addMenuItem(String dName, Double price, Int timeTM, String[] ing, String[] tags){
+    BufferedWriter bw = new BufferedWriter(new FileWriter(menuFile, true));
+    dName += "|";
+    String priceStr = String.valueOf(price) + "|";
+    String timeTMStr = String.valueOf(timeTM) + "|";
+
+    bw.newLine();
+    bw.write(dName);
+    bw.write(priceStr);
+    bw.write(timeTMStr);
+
+    for(int i=0; i<ing.length(); i++){
+      String ingSeperate = ing[i];
+
+      if(i == (ing.length()-1))
+        {ingSeperate += "|";}
+      else
+        {ingSeperated += ",";}
+      bw.write(ingSeperate);
+    }
+
+    for(int j=0; j<tags.length(); i++){
+      String tagsSeperate = tags[j];
+
+      if(j == (tags.length()-1))
+        {tagsSeperate += "|";}
+      else
+        {tagsSeperate += ",";}
+      bw.write(tagsSeperate);
+    }
+
+    bw.close();
   }
 
   /*  Searches the menu for a specfic item.
   It compares the name of each dish to the searched term and adds any matching
   dishes to a linked list, which gets returned.
   */
-  public static LinkedList<Dish> searchMenu(String searchTerm){
+  public static LinkedList<Dish> searchByTerm(String searchTerm){
     LinkedList<Dish> foundDishes = new LinkedList<Dish>();
 
     for(int i = 0; i < menu.size(); i++){
@@ -48,5 +112,25 @@ public class Menu {
     return foundDishes;
   }
 
-  public static LinkedList<Dish> searchIngredients(String[] wantIng){}
+  /*  Searches the menu for any items containing specified ingredients*/
+  public static LinkedList<Dish> searchByIngredients(String[] wantedI){
+    LinkedList<Dish> foundDishes = new LinkedList<Dish>();
+    int counter = menu.size() -1;
+    String[] dishI = new String[];
+
+    while(counter >=0){
+      Dish currentDish = menu.get(counter);
+      dishI = currentDish.getIngredients();
+
+      for(int x = 0; x < wantedI.length; x++){
+        for(int y = 0; y < dishI.length; y++){
+          if(dishI[y].compareToIgnoreCase(wantedI[x])){
+            foundDishes.add(currentDish);
+          }
+        }
+      }
+      counter--;
+    }
+    return foundDishes;
+  }
 }
