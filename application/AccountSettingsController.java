c2d2e3f5a3f15d1eb7cb221d937058c9c5f95c4d
Azurith <34137769+Aften@users.noreply.github.com>
2022-03-29 22:08:41 -0700

    Ver. 1.00

---
+++ application/AccountSettingsController.java
@@ -0,0 +1,47 @@
+package application;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+
+
+public class AccountSettingsController {
+	// Initializes scenes.
+	private Stage stage;
+	private Scene AccountSettingsScene;
+	private Parent root;
+	
+	public Scene returnAccountSettingsScene() {
+		return AccountSettingsScene;
+	}
+	
+}
