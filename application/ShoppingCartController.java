d538af519f8a9bb9a5737b3b21384a119aa075f6
Dombarooni <darodr34@asu.edu>
2022-04-01 15:17:54 -0700

    ShoppingCartStuff

--- application/ShoppingCartController.java
+++ application/ShoppingCartController.java
@@ -1,6 +1,6 @@
 package application;
 import javafx.event.EventHandler;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
@@ -24,15 +24,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 import javax.imageio.IIOParam;
+import java.io.*;
+import java.util.ArrayList;
+
 
 public class ShoppingCartController extends Controller{
 
@@ -45,6 +42,7 @@ public class ShoppingCartController extends Controller{
     // Instantiates the cartList function.
     @FXML
     public ListView<String> cartList;
+    //ObservableList<String> shoppingList = FXCollections.observableArrayList();
 
     // Declares an empty arrayList that will populate the shopping cart.
     ArrayList<String> menuList = new ArrayList<>(
@@ -53,11 +51,81 @@ public class ShoppingCartController extends Controller{
     // Function to add specific item to the shoppingCart
     public void updateShoppingCart() {
         menuList.add("Success");
+        menuList.add("My cock is hard asf rn");
+        menuList.add("Daddy pls");
+        menuList.add("i eat ass");
         cartList.getItems().addAll(menuList);
     }
 
     @FXML
     private Button loginScreenButton;
+    @FXML
+    private TextField myTextField4;
+    @FXML
+    private TextField myTextField;
+    @FXML
+    private TextField myTextField2;
+    @FXML
+    private TextField myTextField3;
+    @FXML
+    private Button myButton;
+    String cardName;
+    String cardNumber;
+    String cardDate;
+    String cardCvv;
+    String newFile = "GwynethPaltrowsCandle.csv";
+    ArrayList<String> card = new ArrayList<String>();
+
+     /*saves input into a text file in array format and comma format
+     * also adds each part of the card information to an array list in the order
+     * cardName cardnumber carddate cardcvv*/
+    public void submit(ActionEvent event)
+    {
+        cardName = myTextField4.getText();
+        cardNumber = myTextField.getText();
+        cardDate = myTextField2.getText();
+        cardCvv = myTextField3.getText();
+
+        card.add(cardName);
+        card.add(cardNumber);
+        card.add(cardDate);
+        card.add(cardCvv);
+        try{
+            //String text = cardNumber + ";" + cardDate + ";" + cardCvv + "\n";
+            //System.out.println(cardNumber+ ";" + cardDate + ";" + cardCvv + "\n");
+            FileWriter fWriter = new FileWriter(newFile, true);
+            BufferedWriter bWriter = new BufferedWriter(fWriter);
+            bWriter.write(String.valueOf(card) + "\n");
+            bWriter.close();
+                System.out.println(card + "\n");
+                card.clear();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    private Label myLabel;
+    @FXML
+    void removeCart(MouseEvent event) {
+    int selectedItem = cartList.getSelectionModel().getSelectedIndex();
+    // if selected == 0
+    if (selectedItem < 1)
+    {
+        myLabel.setText("Nothing Selected");
+    }else{
+        cartList.getItems().remove(selectedItem);
+        myLabel.setText("");
+    }
+    }
+
+    @FXML
+    public void clearCart(ActionEvent event) {
+
+        System.out.println(menuList);
+    }
+
+
 
 }
 
