b9b4c89cccd0b9bdbdc932ace517bbd5d00f9c6a
Laith Abou Saleh <laithas.007@gmail.com>
2022-04-02 05:35:50 -0700

    Added some login functionality

--- profiles/CommonU.java
+++ profiles/CommonU.java
@@ -8,11 +8,20 @@ public class CommonU extends Profile {
     public ArrayList<String> orderHistory;     // ArrayList to add and delete orders
     public int couponCounter;
 
+    // Default constructor
     public CommonU(){
         super();
         isAdmin = false;
     }
 
+    // Constructor to initialize only userName and passWord for user instance
+    public CommonU(String userName, String passWord){
+        uName = userName;
+        pwd = passWord;
+        isAdmin = false;
+    }
+
+    // Constructor to initialize all fields of user instance
     public CommonU(String userName, String passWrod,
                    String fName, String lName, String addr, String number) {
         super(userName, passWrod, fName, lName, addr, number);
