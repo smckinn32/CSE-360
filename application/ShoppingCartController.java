bbd200f6d77bd838ede5bc7b41aa4bb802039d44
Dombarooni <darodr34@asu.edu>
2022-04-03 16:38:12 -0700

    Shopping cart functionality

--- application/ShoppingCartController.java
+++ application/ShoppingCartController.java
@@ -10,6 +10,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,6 +29,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javax.imageio.IIOParam;
@@ -38,17 +40,19 @@ public class ShoppingCartController extends PipeLine {
     @FXML
     private Button loginScreenButton;
     @FXML
-    private TextField myTextField4;
+    private TextField cardNameField;
     @FXML
-    private TextField myTextField;
+    private TextField cardNumField;
     @FXML
-    private TextField myTextField2;
+    private TextField cardDateField;
     @FXML
-    private TextField myTextField3;
+    private TextField cardCvvField;
     @FXML
     private Button myButton;
     @FXML
     private Label myLabel;
+    @FXML
+    private Label cardError;
     // Declarations
     private IIOParam loader;
     private Parent root;
@@ -65,36 +69,63 @@ public class ShoppingCartController extends PipeLine {
         cartListView.getItems().addAll(cartArray);
         System.out.print(cartArray);
     }
+    //declarations
     String cardName;
     String cardNumber;
     String cardDate;
     String cardCvv;
     String newFile = "paymentInfo.csv";
+    BigInteger numberCheck;
     ArrayList<String> card = new ArrayList<String>();
 
     /*saves input into a text file in array format and comma format
      * also adds each part of the card information to an array list in the order
      * cardName cardnumber carddate cardcvv*/
     public void submit(ActionEvent event) {
-        cardName = myTextField4.getText();
-        cardNumber = myTextField.getText();
-        cardDate = myTextField2.getText();
-        cardCvv = myTextField3.getText();
-
-        card.add(cardName);
-        card.add(cardNumber);
-        card.add(cardDate);
-        card.add(cardCvv);
+        cardName = cardNameField.getText();
+        cardNumber = cardNumField.getText();
+        cardDate = cardDateField.getText();
+        cardCvv = cardCvvField.getText();
+    /*bunch of conditionals to make sure user input meets card information requirements*/
         try {
-            //String text = cardNumber + ";" + cardDate + ";" + cardCvv + "\n";
-            //System.out.println(cardNumber+ ";" + cardDate + ";" + cardCvv + "\n");
-            FileWriter fWriter = new FileWriter(newFile, true);
-            BufferedWriter bWriter = new BufferedWriter(fWriter);
-            bWriter.write(String.valueOf(card) + "\n");
-            bWriter.close();
-            System.out.println(card + "\n");
-            card.clear();
-        } catch (Exception e) {
+            numberCheck = new BigInteger(cardNumber + cardDate + cardCvv);
+            try {
+            if (cardNumber.length() != 16) {
+                cardError.setText("Your Card number is incorrect");
+                cardError.setTextFill(Color.color(1,0,0));
+            } else if (cardDate.length() != 5){
+                cardError.setText("Your Card date is incorrect");
+                cardError.setTextFill(Color.color(1,0,0));
+            } else if(cardCvv.length() != 3)
+            {
+                cardError.setText("Your CVV is incorrect");
+                cardError.setTextFill(Color.color(1,0,0));
+            }else {
+                card.add(cardName);
+                card.add(cardNumber);
+                card.add(cardDate);
+                card.add(cardCvv);
+                cardError.setTextFill(Color.color(0,1,0));
+                cardError.setText("Payment information saved");
+                /* Writes information from text fields to file (.csv) */
+                FileWriter fWriter = new FileWriter(newFile, true);
+                BufferedWriter bWriter = new BufferedWriter(fWriter);
+                bWriter.write(String.valueOf(card) + "\n");
+                bWriter.close();
+                System.out.println(card + "\n");
+                card.clear();
+            }
+            } catch (Exception e)
+            {
+            System.out.println(e);
+            }
+        } catch (NumberFormatException e)
+        {
+            cardError.setText("Character not recognized. Numbers only please");
+            cardError.setTextFill(Color.color(1,0,0));
+        }
+        catch (Exception e)
+        {
             System.out.println(e);
         }
     }
